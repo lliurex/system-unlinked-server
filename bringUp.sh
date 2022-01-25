391180b8f5e38216e15291ba3643c142a9ef7619
lliurex <juanma1980@gmail.com>
2022-01-25 16:30:14 +0100

    Ask for variables folder (llx21 n4d vars path)

--- bringUp.sh
+++ bringUp.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
-INTERNAL_IFACE='/var/lib/n4d/variables-dir/INTERNAL_INTERFACE'
+INTERNAL_IFACE='/var/lib/n4d/variables/INTERNAL_INTERFACE'
+if [ ! -e $INTERNAL_FACE ]
+then
+	INTERNAL_IFACE='/var/lib/n4d/variables-dir/INTERNAL_INTERFACE'
+fi
+
 IFACE=""
 
 if [ -e $INTERNAL_IFACE ]
 then
 	#Get internal iface from n4d
-	IFACE=$(grep value $INTERNAL_IFACE | cut -d ":" -f2 | tr -d \"\ )
+	IFACE=$(grep value $INTERNAL_IFACE | cut -d ":" -f2 | tr -d \"\ | tr -d ,)
 fi
-
 
 if [ ! -z $IFACE ]
 then
 
 	PROPOSED_IP=$(netplan-query $IFACE ip)
 	LINKSPEED=$(cat /sys/class/net/$IFACE/speed 2>/dev/null)
-	if [ -z $LINKSPEED ] || [ $LINKSPEED -lt 0 ]
+
+	if [ -z $LINKSPEED ]
 	then
 		#If no speed then there is no link so bring up iface
 		ip addr add $PROPOSED_IP dev $IFACE
